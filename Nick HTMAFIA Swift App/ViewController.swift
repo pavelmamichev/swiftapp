ced58ed20254b1af9346f330a060078c721a136f
Pavel Mamichev <pavel.mamichev@gmail.com>
2016-12-14 02:24:43 +0000

    Added boolean

--- Nick HTMAFIA Swift App/ViewController.swift
+++ Nick HTMAFIA Swift App/ViewController.swift
@@ -9,32 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
-
+    
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        theLabel.text = "Answer: \(Double(text1.text!)! + (Double(text2.text!)!))"
+        let addition = true
+        
+        if addition {
+            
+            theLabel.text = "Answer is.. \(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "Answer is.. \(Double(text1.text!)! - Double(text2.text!)!)"
+        }
+        
         
     }
-
-
-
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
