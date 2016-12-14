4d7d0a38d0b041a7629aa1a4373daa54ad990c21
Pavel Mamichev <pavel.mamichev@gmail.com>
2016-12-14 01:19:14 +0000

    My first save

--- Nick HTMAFIA Swift App/ViewController.swift
+++ Nick HTMAFIA Swift App/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Stop poking me!"
+        print("Button tapped")
+        
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            theLabel.text = ("Congrats! You've pushed the button 10 times") 
+        }
+    }
+    
+    @IBAction func buttonSqueezed(_ sender: Any) {
+        theLabel.text = "Hehe, that tickles."
+        print("Button Squeezed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
